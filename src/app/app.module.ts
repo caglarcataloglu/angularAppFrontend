70081eaf87ea4bf5f3f948698781ed16ec25954b
crozwise <caglarcataloglu@gmail.com>
2022-03-14 21:29:45 +0300

    added URL API endpoint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { FormsModule }   from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomePageComponent,
-    LoginPageComponent
+    LoginPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule //added
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service'; //added
+import { HttpClient } from '@angular/common/http'; //added
+
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -7,16 +9,25 @@ import { AuthService } from '../auth.service'; //added
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { } 
+  constructor(private http: HttpClient) { } //added
   ngOnInit(): void {
   }
 
+
   onSubmit(): void {
-    // to be implemented
-    // rest api calls 
-    alert("Entered Email id :");
     
-  }
+    // rest api calls to nest.js  http://localhost:3000/auth/ //http://localhost:3000/getHello()
+   //npm run start:dev
+    this.http.get<any>('http://localhost:3333/auth/signin').subscribe({
+      next: data => {
+        
+        debugger;console.log(data.total);
+    },
+        error: error => {
+          return { msg: 'Hello Error'};
+        debugger;console.error('There was an error!', error);
+    }
+})}
 
 
 }
